91dc95b7b3f395f6e0896f981dbd95fcf8b2f9c9
Sunil Rana <sunilrana@Sunils-MacBook-Pro.local>
2018-02-01 17:18:32 -0500

    success page

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/ConfirmationActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by natarajan on 02/02/18.
+ */
+
+public class ConfirmationActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_confirmation);
+    }
+}
